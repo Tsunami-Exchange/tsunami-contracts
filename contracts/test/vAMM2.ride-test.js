bb14e4473ec596b6782a741d3a5f691d6b2167b1
dvasin <dvasin@wavesplatform.com>
2022-06-29 22:49:16 +0300

    P2P funding exchange

--- contracts/test/vAMM2.ride-test.js
+++ contracts/test/vAMM2.ride-test.js
@@ -6,6 +6,37 @@ const neutrinoStaking = "3N9LkJahTMx41wGhSxLS42prCZtRCp4dhTs"
 
 const SEED_USDN_HOLDER = "inspire slam drum produce flee force fee false sunset give kidney illegal leave gallery story"
 
+async function stateChangesMap(txId) {
+    const index = {}
+    const changeList = await stateChanges(txId)
+    changeList.data.forEach(e => {
+        index[e.key] = e.value
+    })
+    return index
+}
+
+async function getPositionActualMargin(trader) {
+     const invokeTx = invokeScript({
+        dApp: address(accounts.wallet),
+        call: {
+            function: "view_calcRemainMarginWithFundingPayment",
+            args: [
+                { type: 'string', value: trader }
+            ]
+        },
+    }, accounts.admin);
+
+    try {
+        await broadcast(invokeTx);
+    } catch (e) {
+        let { message } = JSON.parse(JSON.stringify(e))
+        let parts = message.replace('Error while executing account-script: ', '').split(',')
+        let margin = parseInt(parts[0])
+        console.log(`fundingPaymen=${parseInt(parts[1])}`)
+        return margin
+    }
+}
+
 describe('vAMM should work with positive fundind', async function () {
 
     this.timeout(600000);
@@ -27,7 +58,8 @@ describe('vAMM should work with positive fundind', async function () {
             shorter: 0.05 * wvs,
             insurance: 0.05 * wvs,
             staking: 0.05 * wvs,
-            longer: 0.05 * wvs
+            longer: 0.05 * wvs,
+            oracle: 0.01 * wvs
         });
 
         console.log(`Admin address is ${address(accounts.admin)}, public key is ${publicKey(accounts.admin)}`)
@@ -35,20 +67,35 @@ describe('vAMM should work with positive fundind', async function () {
 
         let p1 = deploy('coordinator.ride' , 3400000, accounts.coordinator , 'Coordinator')
         let p2 = deploy('insurance.ride'   , 3400000, accounts.insurance   , 'Insurance Fund')
-        let p3 = deploy('vAMM2.ride'       , 3400000, accounts.wallet      , 'vAMM')
+        let p3 = deploy('vAMM2.ride'       , 4500000, accounts.wallet      , 'vAMM')
 
-        await Promise.all([p1, p2, p3])
+        // Set oracle price to 50 USDN / Waves (AMM price is approx 55)
+        //
+        let seedOracleTx = data({
+            data: [
+                {
+                    "key": "price",
+                    "type": "integer",
+                    "value": Math.round(50 * decimals)
+                }
+            ]
+        }, accounts.oracle)
+
+        await broadcast(seedOracleTx)
+        console.log(`Seed oracle in ${seedOracleTx.id}`)
+
+        await Promise.all([p1, p2, p3, seedOracleTx])
         // Init coordinator
         {
             console.log(`Setting admin...`)
             const addAdminTx = await invoke({
                 dApp: address(accounts.coordinator),
                 functionName: "setAdmin",
-                arguments: [ address(accounts.admin), publicKey(accounts.admin) ],
+                arguments: [ publicKey(accounts.admin) ],
             }, accounts.admin)
 
-            await waitForTx(addAdminTx.id)
             console.log(`setAdmin in ${addAdminTx.id}`)
+            await waitForTx(addAdminTx.id)
 
             const setInsuranceFundTx = await invoke({
                 dApp: address(accounts.coordinator),
@@ -57,8 +104,7 @@ describe('vAMM should work with positive fundind', async function () {
             }, accounts.admin)
 
             console.log(`setInsuranceFund in ${setInsuranceFundTx.id}`)
-            await waitForTx(setInsuranceFundTx.id)
-
+            
             const setQuoteAssetTx = await invoke({
                 dApp: address(accounts.coordinator),
                 functionName: "setQuoteAsset",
@@ -66,7 +112,6 @@ describe('vAMM should work with positive fundind', async function () {
             }, accounts.admin)
 
             console.log(`setQuoteAsset in ${setQuoteAssetTx.id}`)
-            await waitForTx(setQuoteAssetTx.id)
 
             const setStakingAddressTx = await invoke({
                 dApp: address(accounts.coordinator),
@@ -75,7 +120,6 @@ describe('vAMM should work with positive fundind', async function () {
             }, accounts.admin)
 
             console.log(`setStakingAddress in ${setStakingAddressTx.id}`)
-            await waitForTx(setStakingAddressTx.id)
 
             const addAmmTx = await invoke({
                 dApp: address(accounts.coordinator),
@@ -84,6 +128,10 @@ describe('vAMM should work with positive fundind', async function () {
             }, accounts.admin)
 
             console.log(`addAmm in ${addAmmTx.id}`)
+
+            await waitForTx(setInsuranceFundTx.id)
+            await waitForTx(setQuoteAssetTx.id)
+            await waitForTx(setStakingAddressTx.id)
             await waitForTx(addAmmTx.id)
         }
         
@@ -94,16 +142,18 @@ describe('vAMM should work with positive fundind', async function () {
                 call: {
                     function: "initialize",
                     args: [
-                        { type: 'integer', value: 100000 * decimals },  // _quouteAssetReserve
-                        { type: 'integer', value: 1818  * decimals },   // _baseAssetReserve
-                        { type: 'integer', value: 60 },                 // _fundingPeriod = 1 minute
-                        { type: 'integer', value: 0.05 * decimals },    // _initMarginRatio
-                        { type: 'integer', value: 0.1 * decimals },     // _maintenanceMarginRatio
-                        { type: 'integer', value: 0.05 * decimals },    // _liquidationFeeRatio
-                        { type: 'integer', value: 0.01 * decimals },    // _fee 1%
-                        { type: 'string' , value: '3N4NS7d4Jo9a6F14LiFUKKYVdUkkf2eP4Zx' },    // _fee 1%
-                        { type: 'string' , value: 'price' },            // _fee 1%,
-                        { type: 'string' , value: address(accounts.coordinator) },            // _fee 1%
+                        { type: 'integer', value: 100000 * decimals },   // _quouteAssetReserve
+                        { type: 'integer', value: 1818   * decimals },   // _baseAssetReserve ~ 55 USDN / Waves (Optimistic!)
+                        { type: 'integer', value: 60 },                  // _fundingPeriod = 1 minute
+                        { type: 'integer', value: 0.05  * decimals },    // _initMarginRatio = 5%
+                        { type: 'integer', value: 0.085 * decimals },    // _maintenanceMarginRatio = 10%
+                        { type: 'integer', value: 0.05  * decimals },    // _liquidationFeeRatio = 5%
+                        { type: 'integer', value: 0.01  * decimals },    // _fee 1%
+                        { type: 'string' , value: address(accounts.oracle) },    // Oracle address
+                        { type: 'string' , value: 'price' },                                  // Oracle key
+                        { type: 'string' , value: address(accounts.coordinator) },            // Coordinator address,
+                        { type: 'integer', value: 0.1   * decimals },    // _spreadLimit 10%
+                        { type: 'integer', value: 0.08  * decimals },    // _maxPriceImpact 8%
                     ]
                 },
             }, accounts.admin);
@@ -155,7 +205,7 @@ describe('vAMM should work with positive fundind', async function () {
         }
     });
 
-    it('Can add insurance funds', async function () {
+    it.skip('Can add insurance funds', async function () {
         const fundAdminTx = transfer({
             assetId: neutrino,
             amount: 1 * decimals,
@@ -188,7 +238,7 @@ describe('vAMM should work with positive fundind', async function () {
         const openPositionTx = invokeScript({
             dApp: address(accounts.wallet),
             call: {
-                function: "increasePosition",
+                   function: "increasePosition",
                 args: [
                     { type: 'integer', value: 1 }, // _direction = LONG
                     { type: 'integer', value: 3 * decimals }, // _leverage = 3
@@ -207,6 +257,16 @@ describe('vAMM should work with positive fundind', async function () {
         await waitForTx(openPositionTx.id);
 
         console.log('Position opened by ' + openPositionTx.id)
+
+        let changes = await stateChangesMap(openPositionTx.id)
+        
+        expect(changes[`k_positionSize_${address(accounts.longer)}`]).to.be.eq(539786)
+        expect(changes[`k_positionMargin_${address(accounts.longer)}`]).to.be.eq(9900000) // 10 USDN - 1%
+        expect(changes[`k_positionOpenNotional_${address(accounts.longer)}`]).to.be.eq(29700000) // 9900000 * 3
+        
+        expect(changes[`k_totalPositionSize`]).to.be.eq(539786)
+        expect(changes[`k_totalLongPositionSize`]).to.be.eq(539786)
+        expect(changes[`k_totalShortPositionSize`]).to.be.eq(0)
     })
 
     it('Can increase position', async function () {
@@ -232,6 +292,16 @@ describe('vAMM should work with positive fundind', async function () {
         await waitForTx(openPositionTx.id);
 
         console.log('Position increased by ' + openPositionTx.id)
+
+        let changes = await stateChangesMap(openPositionTx.id)
+
+        expect(changes[`k_positionSize_${address(accounts.longer)}`]).to.be.eq(809559)
+        expect(changes[`k_positionMargin_${address(accounts.longer)}`]).to.be.eq(14850000) // 10 USDN - 1% + 5 USDN - 1%
+        expect(changes[`k_positionOpenNotional_${address(accounts.longer)}`]).to.be.eq(44550000) // 14850000 * 3
+        
+        expect(changes[`k_totalPositionSize`]).to.be.eq(809559)
+        expect(changes[`k_totalLongPositionSize`]).to.be.eq(809559)
+        expect(changes[`k_totalShortPositionSize`]).to.be.eq(0)
     })
 
     it('Can decrease position', async function () {
@@ -252,6 +322,16 @@ describe('vAMM should work with positive fundind', async function () {
         await waitForTx(decreasePositionTx.id);
 
         console.log('Position decreased by ' + decreasePositionTx.id)
+
+        let changes = await stateChangesMap(decreasePositionTx.id)
+
+        expect(changes[`k_positionSize_${address(accounts.longer)}`]).to.be.eq(646070)
+        expect(changes[`k_positionMargin_${address(accounts.longer)}`]).to.be.eq(14850007) // Margin stays the same (a bit more due to partially realized p/l)
+        expect(changes[`k_positionOpenNotional_${address(accounts.longer)}`]).to.be.eq(35550007) // Notional is decreased
+        
+        expect(changes[`k_totalPositionSize`]).to.be.eq(646070)
+        expect(changes[`k_totalLongPositionSize`]).to.be.eq(646070)
+        expect(changes[`k_totalShortPositionSize`]).to.be.eq(0)
     })
 
     it('Can add margin', async function () {
@@ -273,6 +353,12 @@ describe('vAMM should work with positive fundind', async function () {
         await waitForTx(addMarginTx.id);
 
         console.log('Added position margin in: ' + addMarginTx.id)
+
+        let changes = await stateChangesMap(addMarginTx.id)
+
+        expect(changes[`k_positionSize_${address(accounts.longer)}`]).to.be.eq(646070) // Do not change size
+        expect(changes[`k_positionMargin_${address(accounts.longer)}`]).to.be.eq(17820007) // Add 3 USDN - 1% to margin
+        expect(changes[`k_positionOpenNotional_${address(accounts.longer)}`]).to.be.eq(35550007) // Notional unchanged
     })
 
     it('Can remove margin', async function () {
@@ -290,6 +376,12 @@ describe('vAMM should work with positive fundind', async function () {
         await waitForTx(removeMarginTx.id);
 
         console.log('Removed position margin in: ' + removeMarginTx.id)
+
+        let changes = await stateChangesMap(removeMarginTx.id)
+
+        expect(changes[`k_positionSize_${address(accounts.longer)}`]).to.be.eq(646070) // Do not change size
+        expect(changes[`k_positionMargin_${address(accounts.longer)}`]).to.be.eq(15820007) // Add 3 USDN - 1% to margin
+        expect(changes[`k_positionOpenNotional_${address(accounts.longer)}`]).to.be.eq(35550007) // Notional unchanged
     })
 
     it('Can open short position', async function () {
@@ -315,6 +407,46 @@ describe('vAMM should work with positive fundind', async function () {
         await waitForTx(openPositionTx.id);
 
         console.log('Position (short) opened by ' + openPositionTx.id)
+
+        let changes = await stateChangesMap(openPositionTx.id)
+
+        expect(changes[`k_positionSize_${address(accounts.shorter)}`]).to.be.eq(-269821)
+        expect(changes[`k_positionMargin_${address(accounts.shorter)}`]).to.be.eq(4950000) // 5 USDN - 1%
+        expect(changes[`k_positionOpenNotional_${address(accounts.shorter)}`]).to.be.eq(14850000) // 3 * (5 USDN - 1%) 
+        
+        expect(changes[`k_totalPositionSize`]).to.be.eq(376249) // 646070 - 269821
+        expect(changes[`k_totalLongPositionSize`]).to.be.eq(646070)
+        expect(changes[`k_totalShortPositionSize`]).to.be.eq(269821)
+    })
+
+    it('Can decrease short position', async function () {
+        const decreasePositionTx = invokeScript({
+            dApp: address(accounts.wallet),
+            call: {
+                function: "decreasePosition",
+                args: [
+                    { type: 'integer', value: 1 }, // _direction = LONG
+                    { type: 'integer', value: 1 * decimals }, // _amount = 1
+                    { type: 'integer', value: 3 * decimals }, // _leverage = 3
+                    { type: 'integer', value: 0.04 * decimals }, // _minBaseAssetAmount = 0.06 WAVES
+                ]
+            }
+        }, accounts.shorter)
+
+        await broadcast(decreasePositionTx);
+        await waitForTx(decreasePositionTx.id);
+
+        console.log('Position (short) decreased by ' + decreasePositionTx.id)
+
+        let changes = await stateChangesMap(decreasePositionTx.id)
+
+        expect(changes[`k_positionSize_${address(accounts.shorter)}`]).to.be.eq(-215305)
+        expect(changes[`k_positionMargin_${address(accounts.shorter)}`]).to.be.eq(4949996) // 5 USDN - 1%
+        expect(changes[`k_positionOpenNotional_${address(accounts.shorter)}`]).to.be.eq(11850004) // 3 * (5 USDN - 1%) 
+        
+        expect(changes[`k_totalPositionSize`]).to.be.eq(430765) // 646070 - 215305
+        expect(changes[`k_totalLongPositionSize`]).to.be.eq(646070)
+        expect(changes[`k_totalShortPositionSize`]).to.be.eq(215305)
     })
 
     it('Can pay funding', async function () {
@@ -323,10 +455,16 @@ describe('vAMM should work with positive fundind', async function () {
 
         console.log(`quouteAssetReserve=${JSON.stringify(quouteAssetReserve)}`)
         console.log(`baseAssetReserve=${JSON.stringify(baseAssetReserve)}`)
+        console.log(`Start waiting 1 block`)
 
         await waitNBlocks(1, {
-            timeout: 100000
+            timeout: 1000000
         })
+
+        console.log(`Done waiting 1 block`)
+
+        const longerOldMargin = await getPositionActualMargin(address(accounts.longer))
+        const shorterOldMargin = await getPositionActualMargin(address(accounts.shorter))
 
         const payFundingTx = invokeScript({
             dApp: address(accounts.wallet),
@@ -337,10 +475,35 @@ describe('vAMM should work with positive fundind', async function () {
         }, accounts.admin)
 
         await broadcast(payFundingTx);
-
         console.log('Paid funding tx: ' + payFundingTx.id)
-
         await waitForTx(payFundingTx.id);
+
+        // Premium is approximately 55 * decimal - 50 * decimal
+        // PremiumFraction is premium * 60 / 86400 = ~31250
+        // Since index price < mark price we need to push AMM price DOWN
+        // to push AMM price DOWN we need to stimulate SHORTS, e.t. long pays SHORT
+        // that means that longer will have a penalty of 0.03125 USDN per waves Holding
+        // longer have 0.64607 Waves 0.64607 * 0.03125 = 0.020 USDN in funding payment
+        // shorter on the other hand should receive 0.020 USDN in funding
+        //
+        const longerNewMargin = await getPositionActualMargin(address(accounts.longer))
+        const shorterNewMargin = await getPositionActualMargin(address(accounts.shorter))
+
+        console.log(JSON.stringify({
+            longerOldMargin,
+            longerNewMargin,
+            shorterOldMargin,
+            shorterNewMargin
+        }, null, 2))
+
+        // Check that longer balance is less now
+        //
+        expect(longerNewMargin).to.be.lt((longerOldMargin))
+        expect(shorterNewMargin).to.be.gt((shorterOldMargin))
+
+        // Expect same diff
+        //
+        expect(longerOldMargin - longerNewMargin).to.be.eq(shorterNewMargin - shorterOldMargin)
     })
 
     it('Can close position', async function () {
@@ -355,6 +518,12 @@ describe('vAMM should work with positive fundind', async function () {
         await waitForTx(closePositionTx.id);
 
         console.log('Position closed by ' + closePositionTx.id)
+
+        let changes = await stateChangesMap(closePositionTx.id)
+
+        expect(changes[`k_totalPositionSize`]).to.be.eq(-215305) // 0 - 215305
+        expect(changes[`k_totalLongPositionSize`]).to.be.eq(0)
+        expect(changes[`k_totalShortPositionSize`]).to.be.eq(215305)
     })
 
     it('Can close short position', async function () {
@@ -369,6 +538,12 @@ describe('vAMM should work with positive fundind', async function () {
         await waitForTx(closePositionTx.id);
 
         console.log('Position closed by ' + closePositionTx.id)
+
+        let changes = await stateChangesMap(closePositionTx.id)
+
+        expect(changes[`k_totalPositionSize`]).to.be.eq(0) 
+        expect(changes[`k_totalLongPositionSize`]).to.be.eq(0)
+        expect(changes[`k_totalShortPositionSize`]).to.be.eq(0)
     })
 })
 
@@ -401,7 +576,7 @@ describe('vAMM should be able to liquidate underwater position', async function 
 
         let p1 = deploy('coordinator.ride' , 3400000, accounts.coordinator , 'Coordinator')
         let p2 = deploy('insurance.ride'   , 3400000, accounts.insurance   , 'Insurance Fund')
-        let p3 = deploy('vAMM2.ride'       , 3400000, accounts.wallet      , 'vAMM')
+        let p3 = deploy('vAMM2.ride'       , 4100000, accounts.wallet      , 'vAMM')
 
         await Promise.all([p1, p2, p3])
         // Init coordinator
@@ -470,6 +645,8 @@ describe('vAMM should be able to liquidate underwater position', async function 
                         { type: 'string' , value: '3N4NS7d4Jo9a6F14LiFUKKYVdUkkf2eP4Zx' },    // _fee 1%
                         { type: 'string' , value: 'price' },            // _fee 1%,
                         { type: 'string' , value: address(accounts.coordinator) },            // _fee 1%
+                        { type: 'integer', value: 0.1   * decimals },    // _spreadLimit 10%
+                        { type: 'integer', value: 0.08  * decimals },    // _maxPriceImpact 8%
                     ]
                 },
             }, accounts.admin);
